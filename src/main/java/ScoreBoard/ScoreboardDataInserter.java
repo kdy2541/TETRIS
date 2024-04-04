381c63df5f3197bbe22b3b8d35d71836c7f3f0f3
ezcho <ezcho886@naver.com>
2024-04-05 03:16:41 +0900

    chore: application 프로퍼티 파일에 jdbc연결 정보 삽입

--- src/main/java/ScoreBoard/ScoreboardDataInserter.java
+++ src/main/java/ScoreBoard/ScoreboardDataInserter.java
@@ -1,21 +1,36 @@
 package ScoreBoard;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Properties;
 
 public class ScoreboardDataInserter {
+    private static final Properties props = new Properties();
 
-    private static final String URL = "jdbc:mysql://18.183.210.86:3306/tetris?useSSL=false";
-    private static final String USER = "ezcho";
-    private static final String PASSWORD = "aD$%$#vveW334";
+    static {
+        loadProperties();
+    }
+
+    private static void loadProperties() {
+        try (InputStream input = ScoreboardDataInserter.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new RuntimeException("application.properties를 찾을 수 없습니다.");
+            }
+            props.load(input);
+        } catch (IOException ex) {
+            throw new RuntimeException("application.properties를 로드하는데 실패했습니다.", ex);
+        }
+    }
 
     public static void insertSampleData() {
         String query = "INSERT INTO scoreboard (nickname, score, time, lines_count, date) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        try (Connection conn = DriverManager.getConnection(props.getProperty("database.url"), props.getProperty("database.user"), props.getProperty("database.password"));
              PreparedStatement pstmt = conn.prepareStatement(query)) {
 
             // 샘플 데이터 삽입
